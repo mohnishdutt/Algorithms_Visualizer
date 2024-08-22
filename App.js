ef27bffea6a452143e96cd8dc7ac07893248c509
mohnishdutt <153698138+mohnishdutt@users.noreply.github.com>
2024-08-22 19:11:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathfinderVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfinderVisualizer></PathfinderVisualizer>
+    </div>
+  );
+}
+
+export default App;
